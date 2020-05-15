8cb6764cdedc840dde1b338026c8176ad747e664
tomhobson <tomhobsonleeds@gmail.com>
2020-05-15 09:18:44 +0100

    Install file for the build from source folks

--- BuildAndInstall.sh
+++ BuildAndInstall.sh
@@ -6,9 +6,9 @@ rm /lib/systemd/system/Sergen.service
 cp Sergen.service /lib/systemd/system/Sergen.service
 
 mkdir -p /opt/Sergen
-dotnet build ../Sergen.sln -c Release
+dotnet build Sergen.sln -c Release
 
-yes | cp -rf ../src/Sergen.Master/bin/Release/netcoreapp3.1/* /opt/Sergen
+yes | cp -rf src/Sergen.Master/bin/Release/netcoreapp3.1/* /opt/Sergen
 
 systemctl daemon-reload
 systemctl start Sergen.service
