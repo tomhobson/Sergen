3388e960fb3f8247674a78762764c4cd305e27b6
tomhobson <tomhobsonleeds@gmail.com>
2020-05-15 09:13:10 +0100

    moved scripts around

---
+++ src/Sergen.Master/Resources/BuildAndInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+systemctl stop Sergen.service
+rm /lib/systemd/system/Sergen.service
+cp Sergen.service /lib/systemd/system/Sergen.service
+
+mkdir -p /opt/Sergen
+dotnet build ../Sergen.sln -c Release
+
+yes | cp -rf ../src/Sergen.Master/bin/Release/netcoreapp3.1/* /opt/Sergen
+
+systemctl daemon-reload
+systemctl start Sergen.service
